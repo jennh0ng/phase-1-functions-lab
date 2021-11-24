e5f33eb4db67baf84a778be5cdbd0e01b5ac2894
Jennifer Hong <hongjenn24@gmail.com>
2021-11-24 15:55:21 -0800

    commit tree f9d9b75a41288cb65876c50b6202323f4e36470f

--- index.js
+++ index.js
@@ -1,62 +1,79 @@
 // Code your solution in this file!
 
-const HQ = 42
+// const HQ = 42
 
-function distanceFromHqInBlocks(pickup) {
-    //returns the number of blocks given a value
+// function distanceFromHqInBlocks(pickup) {
+//     //returns the number of blocks given a value
     
-    if(pickup >= 43) {
-        return (pickup - HQ)
-    }
-    if(pickup < 42) {
-        return (HQ - pickup)
-    }
+//     if(pickup >= 43) {
+//         return (pickup - HQ)
+//     }
+//     if(pickup < 42) {
+//         return (HQ - pickup)
+//     }
+// }
+
+const HQ = 42;
+function distanceFromHqInBlocks(pickUp) {
+    return Math.abs(pickUp-HQ);
 }
 
-function distanceFromHqInFeet(feet) {
-    distanceFromHqInBlocks(feet);
-    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function, 
-    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
-    // the return value of distanceFromHqInBlocks can then be used to calculate feet
-    if(feet >= 43) {
-        return ((feet - HQ) * 264)
-    }
-    if(feet < 42) {
-        return ((HQ - feet) * 264)
-    }
-  }
+// function distanceFromHqInFeet(feet) {
+//     distanceFromHqInBlocks(feet);
+//     // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function, 
+//     // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+//     // the return value of distanceFromHqInBlocks can then be used to calculate feet
+//     if(feet >= 43) {
+//         return ((feet - HQ) * 264)
+//     }
+//     if(feet < 42) {
+//         return ((HQ - feet) * 264)
+//     }
+//   }
+// }
 
-  function distanceTravelledInFeet(start, destination) {
-    //returns the number of feet traveled
-    if (start > destination) {
-        return ((start - destination) * 264)
-    }
-    else if (destination > start) {
-        return ((destination - start) * 264)
-    }
-  }
-
-  distanceTravelledInFeet(42, 50);
+function distanceFromHqInFeet(pickUp) {
+    distanceFromHqInBlocks(pickUp);
+    const distanceF = distanceFromHqInBlocks(pickUp) * 264;
+    return distanceF;
+}
 
 
+//   function distanceTravelledInFeet(start, destination) {
+//     //returns the number of feet traveled
+//     if (start > destination) {
+//         return ((start - destination) * 264)
+//     }
+//     else if (destination > start) {
+//         return ((destination - start) * 264)
+//     }
+//   }
 
-  function calculatesFarePrice(start, destination) {
-    //returns the fare for the customer
-    const ft = distanceTravelledInFeet(start, destination);
+//   distanceTravelledInFeet(42, 50);
 
-    if (ft <= 400) {
-        return 0;
-    }
-    else if (ft > 400 && ft <= 2000) {
-        return (ft - 400) * .02;
-    }
-    else if (ft > 2500) {
-        return 'cannot travel that far';
-    }
-    else if (ft > 2000) {
-        return 25;
-    }
-  }
+function distanceTravelledInFeet(start, end) {
+    const travel = (end - start) * 264;
+    return Math.abs(travel);
+}
+
+
+//   function calculatesFarePrice(start, destination) {
+//     //returns the fare for the customer
+//     const ft = distanceTravelledInFeet(start, destination);
+
+//     if (ft <= 400) {
+//         return 0;
+//     }
+//     else if (ft > 400 && ft <= 2000) {
+//         return (ft - 400) * .02;
+//     }
+//     else if (ft > 2500) {
+//         return 'cannot travel that far';
+//     }
+//     else if (ft > 2000) {
+//         return 25;
+//     }
+//   }
 
 
 
